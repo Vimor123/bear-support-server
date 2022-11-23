2f14cb5d76e02b46277c991f75a5f514a99c37a5
Vimor123 <vmladar1@gmail.com>
2022-11-23 09:31:28 +0100

    Just have to update the look

--- bearsupportserver/src/App.js
+++ bearsupportserver/src/App.js
@@ -16,12 +16,13 @@ function App() {
     }
 
     return (
-        <div>
-            <h1>
-                Hello
-            </h1>
-            <div>
-                <img src={"/bears/" + bearPic} alt={"sexy"}></img>
+        <div className={"content"}>
+            <div className={"head-div"}>
+                <h1 className={"title"}> Bear Support Den </h1>
+                <span> A place where you can look at cute and sexy bears when you're feeling down. </span>
+            </div>
+            <div className={"bear-image-container"}>
+                <img className={"bear-image"} src={"/bears/" + bearPic} alt={"sexy"}></img>
             </div>
             <div>
                 <button onClick={askforNewPic}>NEW BEAR</button>
